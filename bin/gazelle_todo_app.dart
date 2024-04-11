e7b3c5552c6c77e0d20f04a5423184566101b8fc
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-04-11 16:38:22 +0530

    added bin folder

---
+++ bin/gazelle_todo_app.dart
@@ -0,0 +1,5 @@
+import 'package:gazelle_todo_app/gazelle_todo_app.dart';
+
+void main(List<String> arguments) {
+  runServer();
+}
--- lib/gazelle_todo_app.dart
+++ lib/gazelle_todo_app.dart
@@ -1,6 +1,6 @@
 import 'package:gazelle_todo_app/gazelle_service.dart';
 
-void main() async {
+void runServer() async {
   await GazelleService.start();
   print(
       'Server is running at http://${GazelleService.app.address}:${GazelleService.app.port}');
